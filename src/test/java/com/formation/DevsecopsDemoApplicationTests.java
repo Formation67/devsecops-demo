41ccf33edd580e6efb8bbdd84cefec51385b5e1e
Mohamadou Mbengue <mbengue.m@gmail.com>
2022-03-26 10:45:28 +0100

    Add Junit

--- src/test/java/com/formation/DevsecopsDemoApplicationTests.java
+++ src/test/java/com/formation/DevsecopsDemoApplicationTests.java
@@ -1,13 +1,41 @@
 package com.formation;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class DevsecopsDemoApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void smallerThanOrEqualToFiftyMessage() throws Exception {
+		this.mockMvc.perform(get("/compare/49")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string("Smaller than or equal to 50"));
+	}
+
+	@Test
+	public void greaterThanFiftyMessage() throws Exception {
+		this.mockMvc.perform(get("/compare/51")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string("Greater than 50"));
+	}
+
+	@Test
+	public void welcomeMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
 	}
 
 }
